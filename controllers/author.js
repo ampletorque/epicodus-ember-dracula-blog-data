180d0430bfb95e95711339baacc9bbeecec8e15b
Ian MacDonald & Kim Dang <student@epicodus.com>
2015-06-08 10:45:33 -0700

    Add controller for a pop up in the authors page and to send a message in the contact page.

---
+++ controllers/author.js
@@ -0,0 +1,7 @@
+Blogger.AuthorController = Ember.Controller.extend({
+  actions: {
+    showRealName: function() {
+      alert('Dracula is fictional, but was inspired by the 15th-century Romanian general and Wallachian Prince Wlad III the Impaler.')
+    }
+  }
+});
