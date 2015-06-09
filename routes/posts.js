9705d08ea49420708f7554f10d7abe5e845d1cf0
Kim <kimdangg@kim-dangs-MacBook.local>
2015-06-09 09:18:20 -0700

    Add posts in store.js and route the posts.

---
+++ routes/posts.js
@@ -0,0 +1,6 @@
+Blogger.PostsRoute = Ember.Route.extend({
+  controllerName: 'posts',
+  renderTemplate: function() {
+    this.render('posts');
+  }
+});
---
+++ store.js
@@ -0,0 +1,94 @@
+var posts = [
+  {
+    id: '1',
+    title: "I love blood",
+    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+  },
+  {
+    id: '2',
+    title: "Today was a good day",
+    body: "Just waking up in the morning gotta thank God
+      I don't know but today seems kinda odd
+      No barking from the dogs, no smog
+      And momma cooked a breakfast with no hog
+      I got my grub on, but didn't pig out
+      Finally got a call from a girl want to dig out
+      Hooked it up on later as I hit the do'
+      Thinking will i live another twenty fo'
+      I gotta go cause I got me a drop top
+      And if I hit the switch, I can make the ass drop
+      Had to stop at a red light
+      Looking in my mirror not a jacker in sight
+      And everything is alright
+      I got a beep from Kim and she can fuck all night
+      Called up the homies and I'm askin' y'all
+      Which park, are y'all playin' basketball?
+      Get me on the court and I'm trouble
+      Last week fucked around and got a triple double
+      Freaking brothers every way like M.J.
+      I can't believe, today was a good day
+
+      Drove to the pad and hit the showers
+      Didn't even get no static from the cowards
+      Cause just yesterday them fools tried to blast me
+      Saw the police and they rolled right past me
+      No flexin', didn't even look in a niggas direction as I ran the intersection
+      Went to $hort Dog's house, they was watchin' Yo! MTV Raps
+      What's the haps on the craps
+      Shake 'em up, shake 'em up, shake 'em up, shake 'em
+      Roll 'em in a circle of niggaz and watch me break 'em
+      With the seven, seven-eleven, seven-eleven
+      Seven even back do' little Joe
+      I picked up the cash flow
+      Then we played bones, and I'm yellin' domino
+      Plus nobody I know got killed in South Central L.A.
+      Today was a good day
+
+      Left my niggas house paid
+      Picked up a girl been tryin' to fuck since the twelfth grade
+      It's ironic, I had the brew she had the chronic
+      The Lakers beat the Supersonics
+      I felt on the big fat fanny
+      Pulled out the jammy, and killed the punanny
+      And my dick runs deep so deep so deep put her ass to sleep
+      Woke her up around one
+      she didn't hesitate, to call Ice Cube the top gun
+      Drove her to the pad and I'm coasting
+      Took another sip of the potion hit the three-wheel motion
+      I was glad everything had worked out
+      Dropped her ass off, then I chirped out
+      Today was like one of those fly dreams
+      Didn't even see a berry flashing those high beams
+      No helicopter looking for a murder
+      Two in the morning got the fat burger
+      Even saw the lights of the Goodyear Blimp
+      And it read Ice Cube's a pimp (yeah)
+      Drunk as hell but no throwing up
+      Half way home and my pager still blowing up
+      Today I didn't even have to use my A.K.
+      I got to say it was a good day."
+  },
+  {
+    id: '3',
+    title: 'Roaming around in the dark',
+    body: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of 'de Finibus Bonorum et Malorum' (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, 'Lorem ipsum dolor sit amet..', comes from a line in section 1.10.32."
+  }
+];
+
+var comments = [
+  {
+    id: '1',
+    postId: '3',
+    text: 'What a shame!'
+  },
+  {
+    id: '2',
+    postId: '3',
+    text: 'I am so sorry. How are you feeling now?'
+  },
+  {
+    id: '3',
+    postId: '1',
+    text: 'I love blood'
+  }
+]
